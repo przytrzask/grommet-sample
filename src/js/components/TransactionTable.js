9331e0522c0fbfc4a3e697cda1caa73c99c64ce4
trzasQ <przytrzask@gmail.com>
2017-02-19 11:18:37 +0100

    switch to private repo

--- src/js/components/TransactionTable.js
+++ src/js/components/TransactionTable.js
@@ -4,22 +4,6 @@ import React from 'react';
 // import Table from 'grommet/components/Table';
 import TableHeader from 'grommet/components/TableHeader';
 
-// import TableRow from 'grommet/components/TableRow';
-// import Menu from 'grommet/components/Menu';
-// import Anchor from 'grommet/components/Anchor';
-// import NavAnchor from './NavAnchor';
-
-
-
-
-
-
-
-// var links = items.map((page) => {
-//       return (
-//         <Anchor key={page.label} path={page.path} label={page.label} />
-//       );
-//     });
 
 
 
